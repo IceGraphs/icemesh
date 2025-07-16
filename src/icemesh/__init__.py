e6d362ae7f9b44779755a98153c71af256afcfe6
Meiert Grootes <mwgrootes@Meierts-MacBook-Pro.local>
2025-07-16 13:13:15 +0200

    first commit

---
+++ src/icemesh/__init__.py
@@ -0,0 +1,9 @@
+"""Documentation about icemesh."""
+
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Meiert W. Grootes"
+__email__ = "m.grootes@esciencecenter.nl"
+__version__ = "0.0.1"
