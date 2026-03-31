829afaba97e0a4b22c0aeaa14333ede4e91fecfc
Thijs van Lankveld <t.vanlankveld@esciencecenter.nl>
2026-03-31 15:52:52 +0200

    Removed outdated implementations.

--- src/icemesh_PyG/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Documentation about icemesh_PyG."""
-
-import logging
-
-logging.getLogger(__name__).addHandler(logging.NullHandler())
-
-__author__ = "Meiert W. Grootes"
-__email__ = "m.grootes@esciencecenter.nl"
-__version__ = "0.0.1"
